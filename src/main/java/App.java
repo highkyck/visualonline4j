76462fc95a8681c186343b51490bb355a376bc34
liubang <it.liubang@gmail.com>
2018-11-21 18:44:10 +0800

    init project

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+/**
+ * {Insert class description here}
+ *
+ * @author <a href="mailto:liubang@staff.weibo.com">liubang</a>
+ * @version $Revision: {Version} $ $Date: 2018/11/21 18:43 $
+ * @see
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
